cdfccb254aec008e03aa99edaad1978b1063def0
Артём Мариненко <zalgan@mail.ru>
2024-01-18 20:28:52 +0300

    Добавлены модели пользователя с ролями, руководителя, сотрудника, менторство

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Manager, Employee
+
+
+class EmloyeeInline(admin.TabularInline):
+    model = Employee
+    extra = 1
+
+
+@admin.register(Manager)
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = '__all__'
+    inlines = (EmloyeeInline)
--- users/models.py
+++ users/models.py
@@ -1,40 +1,71 @@
-from backend.plans import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+
+MAX_LENGHT = 64
 
 
 class User(AbstractUser):
 
     class Role(models.TextChoices):
-        HEAD = "head"
-        EMPLOYEE = "employee"
+        MANAGER = 'manager'
+        EMPLOYEE = 'employee'
 
-    middle_name = models.CharField(max_length=50)
-
+    first_name = models.CharField(max_lenght=MAX_LENGHT)
+    middle_name = models.CharField(max_lenght=MAX_LENGHT)
+    last_name = models.CharField(max_lenght=MAX_LENGHT)
     role = models.CharField(
-        "Роль",
-        max_length=20,
+        max_lenght=MAX_LENGHT,
         choices=Role.choices,
-        default=Role.EMPLOYEE,
+        default=Role.EMPLOYEE
     )
 
     class Meta:
-        verbose_name = "Сотрудник"
-        verbose_name_plural = "Сотрудники"
+        verbose_name = 'Пользователь'
+        verbose_plural_name = 'Пользователи'
 
 
-class EmployeeProfile(models.Model):
+class Manager(models.Model):
+    """Модель руководителя"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    position = models.CharField(max_length=50, blank=True, null=True)
-    grade = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        verbose_name = 'Профиль сотрудника'
-        verbose_name_plural = 'Профили сотрудников'
+        verbose_name = 'Руководитель'
+        verbose_plural_name = 'Руководители'
 
 
-# class HeadProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Employee(models.Model):
+    """Модель сотрудника"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    position = models.CharField(max_lenght=MAX_LENGHT)
+    grade = models.CharField(max_lenght=MAX_LENGHT)
+    head = models.ForeignKey(User, on_delete=models.CASCADE)
 
-#     class Meta:
-#         verbose_name = 'Профиль руководителя'
-#         verbose_name_plural = 'Профили руководителей'
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_plural_name = 'Сотрудники'
+
+
+class MentorEmployee(models.Model):
+    """Модель для связи ментора из числа сотрудников с сотрудником."""
+    mentor = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    mentee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.mentor == self.mentee.supervisor:
+            raise ValidationError("Сотрудник не может быть своим ментором.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Связь ментор-сотрудник'
+        verbose_plural_name = 'Связи ментор-сотрудник'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('mentor', 'mentee'),
+                name='unique_mentor_mentee'
+            )
+        ]
