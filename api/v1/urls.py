8665f780f8339b0e9828f81e90c0af450f2b5f7b
Артём Мариненко <zalgan@mail.ru>
2024-01-20 18:03:43 +0300

    Добавлен url, viewset

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,7 +1,13 @@
-"""URL-роутинг приложения API."""
-# from django.urls import path, include
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from .views import IDPViewSet
+
+router = DefaultRouter()
+router.register(
+    r"employees/(?P<employee_id>\d+)/idps", IDPViewSet, basename="idps"
+)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
