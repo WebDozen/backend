109a5108e1c6ed522d70330d7030864c2bcb00e0
Данила Мандрейкин <dan.mandreikin@yandex.ru>
2024-01-21 19:18:30 +0300

    Обновление тестов для моделей users, создание тестов для моделей plans.

--- tete.py
+++ tete.py
@@ -4,29 +4,28 @@ from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
 
-class BaseUserModelTest(TestCase):
-    """Базовый тестовый класс для модели User."""
+class UserModelTest(TestCase):
+    """Тестирование модели User."""
 
     def setUp(self):
         """Настройка экземпляра User для тестирования."""
+
         self.user = User.objects.create(
             first_name='John',
             middle_name='Doe',
             last_name='Smith',
             role=User.Role.EMPLOYEE,
-            email='john_doe_employee@example.com'
+            username='john_doe_employee'
         )
-
-
-class UserModelTest(BaseUserModelTest):
-    """Тестирование модели User."""
 
     def test_user_creation(self):
         """Тест успешного создания экземпляра User."""
+
         self.assertIsInstance(self.user, User)
 
     def test_user_str_method(self):
         """Тест строкового представления модели User."""
+
         expected_str = (f'{self.user.first_name}'
                         f'{self.user.middle_name}'
                         f'{self.user.last_name}'
@@ -34,27 +33,34 @@ class UserModelTest(BaseUserModelTest):
         self.assertEqual(str(self.user), expected_str)
 
 
-class BaseManagerModelTest(BaseUserModelTest):
-    """Базовый тестовый класс для модели Manager."""
+class ManagerModelTest(TestCase):
+    """Тестирование модели Manager."""
 
     def setUp(self):
         """Настройка экземпляра Manager для тестирования."""
-        super().setUp()
-        self.user.role = User.Role.MANAGER
-        self.user.email = 'john_doe_manager@example.com'
-        self.user.save()
-        self.manager = Manager.objects.create(user=self.user)
 
-
-class ManagerModelTest(BaseManagerModelTest):
-    """Тестирование модели Manager."""
+        self.manager_user = User.objects.create(
+            first_name='John',
+            middle_name='Doe',
+            last_name='Smith',
+            role=User.Role.MANAGER,
+            username='john_doe_manager'
+        )
+        self.manager = Manager.objects.create(user=self.manager_user)
 
     def test_manager_creation(self):
         """Тест успешного создания экземпляра Manager."""
+
         self.assertIsInstance(self.manager, Manager)
+
+    def test_manager_user_relation(self):
+        """Тест связи пользователя и менеджера."""
+
+        self.assertEqual(self.manager.user, self.manager_user)
 
     def test_manager_str_method(self):
         """Тест строкового представления модели Manager."""
+
         expected_str = (f'Руководитель: {self.manager.user.first_name}'
                         f'{self.manager.user.middle_name}'
                         f'{self.manager.user.last_name}'
@@ -62,36 +68,53 @@ class ManagerModelTest(BaseManagerModelTest):
         self.assertEqual(str(self.manager), expected_str)
 
 
-class BaseEmployeeModelTest(BaseManagerModelTest):
-    """Базовый тестовый класс для модели Employee."""
+class EmployeeModelTest(TestCase):
+    """Тестирование модели Employee."""
 
     def setUp(self):
         """Настройка экземпляра Employee для тестирования."""
-        super().setUp()
-        self.user.role = User.Role.EMPLOYEE
-        self.user.email = 'john_doe_employee@example.com'
-        self.user.save()
+
+        self.manager_user = User.objects.create(
+            first_name='Manager',
+            middle_name='Doe',
+            last_name='Smith',
+            role=User.Role.MANAGER,
+            username='manager_doe'
+        )
+        self.manager = Manager.objects.create(user=self.manager_user)
+
+        self.employee_user = User.objects.create(
+            first_name='John',
+            middle_name='Doe',
+            last_name='Smith',
+            role=User.Role.EMPLOYEE,
+            username='john_doe_employee'
+        )
         self.employee = Employee.objects.create(
-            user=self.user,
+            user=self.employee_user,
             position='Developer',
             grade='Junior',
             head=self.manager
         )
 
-
-class EmployeeModelTest(BaseEmployeeModelTest):
-    """Тестирование модели Employee."""
-
     def test_employee_creation(self):
         """Тест успешного создания экземпляра Employee."""
+
         self.assertIsInstance(self.employee, Employee)
 
     def test_employee_user_relation(self):
         """Тест связи пользователя и сотрудника."""
-        self.assertEqual(self.employee.user, self.user)
+
+        self.assertEqual(self.employee.user, self.employee_user)
+
+    def test_employee_manager_relation(self):
+        """Тест связи сотрудника и руководителя."""
+
+        self.assertEqual(self.employee.head, self.manager)
 
     def test_employee_str_method(self):
         """Тест строкового представления модели Employee."""
+
         expected_str = (f'Сотрудник: {self.employee.user.first_name}'
                         f'{self.employee.user.middle_name}'
                         f'{self.employee.user.last_name}'
@@ -99,18 +122,27 @@ class EmployeeModelTest(BaseEmployeeModelTest):
         self.assertEqual(str(self.employee), expected_str)
 
 
-class BaseMentorEmployeeModelTest(BaseEmployeeModelTest):
-    """Базовый тестовый класс для модели MentorEmployee."""
+class MentorEmployeeModelTest(TestCase):
+    """Тестирование модели MentorEmployee."""
 
     def setUp(self):
         """Настройка экземпляра MentorEmployee для тестирования."""
-        super().setUp()
+
+        self.manager_user = User.objects.create(
+            first_name='Manager',
+            middle_name='Doe',
+            last_name='Smith',
+            role=User.Role.MANAGER,
+            username='manager_doe'
+        )
+        self.manager = Manager.objects.create(user=self.manager_user)
+
         self.mentor_user = User.objects.create(
             first_name='Mentor',
             middle_name='Doe',
             last_name='Smith',
             role=User.Role.EMPLOYEE,
-            email='mentor_doe@example.com'
+            username='mentor_doe'
         )
         self.mentor = Employee.objects.create(
             user=self.mentor_user,
@@ -118,12 +150,13 @@ class BaseMentorEmployeeModelTest(BaseEmployeeModelTest):
             grade='Senior',
             head=self.manager
         )
+
         self.mentee_user = User.objects.create(
             first_name='Mentee',
             middle_name='Doe',
             last_name='Smith',
             role=User.Role.EMPLOYEE,
-            email='mentee_doe@example.com'
+            username='mentee_doe'
         )
         self.mentee = Employee.objects.create(
             user=self.mentee_user,
@@ -132,18 +165,16 @@ class BaseMentorEmployeeModelTest(BaseEmployeeModelTest):
             head=self.manager
         )
 
-
-class MentorEmployeeModelTest(BaseMentorEmployeeModelTest):
-    """Тестирование модели MentorEmployee."""
-
     def test_mentor_employee_creation(self):
         """Тест успешного создания экземпляра MentorEmployee."""
+
         mentor_employee = MentorEmployee.objects.create(mentor=self.mentor,
                                                         mentee=self.mentee)
         self.assertIsInstance(mentor_employee, MentorEmployee)
 
     def test_mentor_employee_relations(self):
         """Тест связей между ментором и сотрудником."""
+
         mentor_employee = MentorEmployee.objects.create(mentor=self.mentor,
                                                         mentee=self.mentee)
         self.assertEqual(mentor_employee.mentor, self.mentor)
@@ -151,6 +182,7 @@ class MentorEmployeeModelTest(BaseMentorEmployeeModelTest):
 
     def test_mentor_employee_clean_method(self):
         """Тест функции clean модели MentorEmployee."""
+
         with self.assertRaises(ValidationError) as context:
             mentor_employee = MentorEmployee(mentor=self.mentee,
                                              mentee=self.mentee)
@@ -163,6 +195,7 @@ class MentorEmployeeModelTest(BaseMentorEmployeeModelTest):
 
     def test_mentor_employee_unique_constraint(self):
         """Тест уникальности связи между ментором и сотрудником."""
+
         MentorEmployee.objects.create(mentor=self.mentor,
                                       mentee=self.mentee)
 
