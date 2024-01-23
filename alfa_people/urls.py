e442b8074e84fb0b2705056c359703be040f523f
Артём Мариненко <zalgan@mail.ru>
2024-01-23 16:24:26 +0300

    Добавлена простейшая аутентификация

--- alfa_people/urls.py
+++ alfa_people/urls.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken import views
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,4 +8,5 @@ VERSION_API = '1'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
